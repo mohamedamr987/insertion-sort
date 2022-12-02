ac255faf053e217af83471287e7ad0f65bee5299
Mohamed Amr <81720517+mohamedamr987@users.noreply.github.com>
2022-12-02 17:44:17 +0200

    created the algorithm

---
+++ bin/insertion_sort.dart
@@ -0,0 +1,19 @@
+List<int> insertionSort(List<int> input){
+  for(int i = 1; i < input.length ; i++){
+    int key = input[i];
+    int j = i - 1;
+    while( j >=0 && key < input[j]){
+      input[j+1] = input[j];
+      j--;
+    }
+    input[j+1] = key;
+  }
+  return input;
+}
+
+void main(List<String> arguments) {
+  List<int> testArray = [4,5,1,3,2];
+  print("Before sorting : $testArray");
+  testArray = insertionSort(testArray);
+  print("After sorting : $testArray");
+}
---
+++ test/insertion_sort_test.dart
@@ -0,0 +1,8 @@
+import 'package:insertion_sort/insertion_sort.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
